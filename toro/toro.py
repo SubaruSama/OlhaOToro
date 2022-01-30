d36e808cbaccff7f0912a7dc431edd9a23c32889
SubaruSama <andresfauque@gmail.com>
2022-01-30 00:37:50 -0300

    Alterando métodos e os requisitos. Mexendo nas anotacoes.txt dnv

--- toro/toro.py
+++ toro/toro.py
@@ -1,4 +1,4 @@
-import urllib3
+import requests
 import discord
 from config import api_key_openweather
 
@@ -16,11 +16,11 @@ class ToroBot(discord.Client):
     def __init__(self, city: str, state: str, country: str) -> None:
         """Construtor."""
         # TODO: deixar que ele receba estes valores de city, state e country via mensagem do Discord
+        super().__init__()
         self.city = city
         self.state = state
         self.country = country
         self.url = f'https://api.openweathermap.org/data/2.5/weather?q={self.city},{self.state},{self.country}&units=metric&appid={api_key_openweather}'
-        super().__init__()
 
     # Discord stuff
     async def on_message(self, message):
@@ -28,8 +28,8 @@ class ToroBot(discord.Client):
         Método herdado da classe discord.Client
         Será chamado quando um model Message é criado e enviado.
         """
-        if message.content.startswith('$vai_chover'): # TODO: Existe o discord.ext.commands, decorators que ajudam no parse. Utilizar depois
-            await message.channel.send('Não sei')
+        if message.content.startswith('$tempo'): # TODO: Existe o discord.ext.commands, decorators que ajudam no parse. Utilizar depois.
+            await message.channel.send(self.get_weather())
 
     async def on_ready(self) -> None:
         """
@@ -49,7 +49,7 @@ class ToroBot(discord.Client):
         pass
 
     # OpeanWeather stuff
-    def it_will_rain(self, contents: str) -> None:
+    def it_will_rain_automatic(self, contents: str) -> None:
         """
         Método que decide se irá chover ou não. Caso true, chama notify_all(). Caso false, faz nada.
         """
@@ -58,10 +58,25 @@ class ToroBot(discord.Client):
         else:
             pass
 
-    def get_weather(self) -> None:
+    def get_weather(self) -> any:
         """
-        Método que pega os dados do OpenWeather e envia para o parse_weather
+        Método que pega os dados do OpenWeather.
+        Este método possui duas funções:
+            Enviar o response para o it_will_rain_automatic, responsável por avisar se vai chover e chamar o notify_all().\n
+            Preencher as variáveis com os valores recebidos do json e envia para o it_will_rain, método que responde quando o comando é enviado.
         """
-        http = urllib3.PoolManager()
-        get = http.request('GET', self.url)
-        self.it_will_rain(get)
+        response = requests.request('GET', self.url).json()
+        # self.it_will_rain_automatic(response)
+        
+        temp = response["main"]["temp"]
+        temp_max = response["main"]["temp_max"]
+        temp_min = response["main"]["temp_min"]
+        feels_like = response["main"]["feels_like"]
+        humidity = response["main"]["humidity"]
+        weather_main = response["weather"][0]["main"]
+        weather_description = response["weather"][0]["description"]
+        payload = f'Temperatura atual: {temp} °C\nTemperatura máxima: {temp_max} °C\nTemperatura mínima: {temp_min} °C\n' \
+            f'Sensação térmica: {feels_like} °C\nUmidade: {humidity}%\n' \
+            f'Tempo: {weather_main}\nDescrição do tempo: {weather_description}'
+
+        return payload
