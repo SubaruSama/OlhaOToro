40fb8fb6a8f8c88fd5134db10495b23459112323
SubaruSama <andresfauque@gmail.com>
2022-01-29 23:39:56 -0300

    Modifiquei alguns métodos e exclui o que achei não mais necessário ou que estava adicionando complexidade

--- toro/toro.py
+++ toro/toro.py
@@ -1,23 +1,26 @@
 import urllib3
 import discord
-import config
+from config import api_key_openweather
 
 class ToroBot(discord.Client):
-    """Classe do Bot.
+    """
+    Classe do Bot.
     Esta classe herda a classe Client. Esta classe contém
     métodos e atributos que facilitam na hora de interagir
-    com o WebSockets e API do Discord"""
+    com o WebSockets e API do Discord
+    """
     bot_name = 'Toro'
     version = 0.1
     author = 'r0se / SubaruSama'
 
     def __init__(self, city: str, state: str, country: str) -> None:
-        self.api_key = config.api_key_discord
+        """Construtor."""
         # TODO: deixar que ele receba estes valores de city, state e country via mensagem do Discord
         self.city = city
         self.state = state
         self.country = country
-        self.url = f'https://api.openweathermap.org/data/2.5/weather?q={self.city},{self.state},{self.country}&units=metric&appid={config.api_key_openweather}'
+        self.url = f'https://api.openweathermap.org/data/2.5/weather?q={self.city},{self.state},{self.country}&units=metric&appid={api_key_openweather}'
+        super().__init__()
 
     # Discord stuff
     async def on_message(self, message):
@@ -25,10 +28,14 @@ class ToroBot(discord.Client):
         Método herdado da classe discord.Client
         Será chamado quando um model Message é criado e enviado.
         """
-        if message.content.startswith('$vai_chover'):
+        if message.content.startswith('$tempo'): # TODO: Existe o discord.ext.commands, decorators que ajudam no parse. Utilizar depois
             await message.channel.send('Nao sei')
 
     async def on_ready(self) -> None:
+        """
+        No momento em que o bot é iniciado, printa no terminal com qual usuário está logado.
+        Para debug apenas.
+        """
         print(f'Logado como {self.user}')
 
     def notify_all(self):
@@ -39,17 +46,19 @@ class ToroBot(discord.Client):
         pass
 
     # OpeanWeather stuff
+    def it_will_rain(self, contents: str) -> None:
+        """
+        Método que decide se irá chover ou não. Caso true, chama notify_all(). Caso false, faz nada.
+        """
+        if contents["weather"][0]["main"] != "Clear":
+            self.notify_all()
+        else:
+            pass
+
     def get_weather(self) -> None:
         """
-        Método que retorna dados a partir do OpenWeather.
+        Método que pega os dados do OpenWeather e envia para o parse_weather
         """
         http = urllib3.PoolManager()
         get = http.request('GET', self.url)
         self.it_will_rain(get)
-
-    def it_will_rain(self, contents) -> bool:
-        """
-        Método que decide se irá chover ou não. Caso true, chama notify_all(). Caso false, faz nada.
-        Precisa realizar um parse do json vindo do método get_weather().
-        """
-        pass
