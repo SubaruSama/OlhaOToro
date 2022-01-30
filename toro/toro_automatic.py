39c497db2380da0e2514022cab55397d60fced08
SubaruSama <andresfauque@gmail.com>
2022-01-30 00:41:41 -0300

    Criando outra classe para cuidar das automações de puxar e avisar

---
+++ toro/toro_automatic.py
@@ -0,0 +1,7 @@
+import toro
+
+class ToroBotAutomatic(toro.ToroBot):
+    """
+    Classe que irá ser responsável por cuidar de puxar os dados e avisar automaticamente quando chover.
+    """
+    pass
